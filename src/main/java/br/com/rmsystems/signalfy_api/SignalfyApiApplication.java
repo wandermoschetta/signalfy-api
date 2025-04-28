ade6a22eaabb908cbd38da2b51bef50073230d55
Wander S. Moschetta <wander.sm@gmail.com>
2025-04-28 15:14:16 -0300

    Start project

---
+++ src/main/java/br/com/rmsystems/signalfy_api/SignalfyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rmsystems.signalfy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalfyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalfyApiApplication.class, args);
+	}
+
+}
